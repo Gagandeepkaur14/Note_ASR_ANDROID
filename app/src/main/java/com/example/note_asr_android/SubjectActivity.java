4e91294cfbbdb13008a12c6f4cac5eccf149243c
BaljeetAmrit <72460561+AmritKaur0955@users.noreply.github.com>
2021-02-23 00:19:49 -0500

    new activity added

--- app/src/main/java/com/example/note_asr_android/SubjectActivity.java
+++ app/src/main/java/com/example/note_asr_android/SubjectActivity.java
@@ -1,6 +1,12 @@
 package com.example.note_asr_android;
 
 import android.app.Activity;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SubjectActivity extends Activity {
+    TextView drawer_txt,new_note,txt_title;
+    RecyclerView recyclerView;
+
 }
