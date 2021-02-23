e65f120a29e5d2d06117cde8a167f2a65a5d13e0
Amandeep98781 <kauraman98781@gmail.com>
2021-02-22 23:57:30 -0500

    new func added

--- app/src/main/java/com/example/note_asr_android/SubjectActivity.java
+++ app/src/main/java/com/example/note_asr_android/SubjectActivity.java
@@ -1,7 +1,15 @@
 package com.example.note_asr_android;
 
 import android.app.Activity;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class SubjectActivity extends Activity {
+    TextView drawer_txt,new_note,txt_title;
+    RecyclerView recyclerView;
+
 
 }
