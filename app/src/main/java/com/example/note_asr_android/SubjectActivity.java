c24b84d15a21015b3c5be5ac07cd4920170cb0d6
BaljeetAmrit <72460561+AmritKaur0955@users.noreply.github.com>
2021-02-23 00:17:56 -0500

    new activity added

--- app/src/main/java/com/example/note_asr_android/SubjectActivity.java
+++ app/src/main/java/com/example/note_asr_android/SubjectActivity.java
@@ -1,6 +1,38 @@
 package com.example.note_asr_android;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class SubjectActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SubjectActivity extends AppCompatActivity {
+
+    TextView drawer_txt, new_note, txt_title;
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject);
+        drawer_txt = (TextView) findViewById(R.id.drawer_icon);
+        new_note = (TextView) findViewById(R.id.new_note);
+        txt_title = (TextView) findViewById(R.id.txt_title);
+
+        recyclerView = (RecyclerView) findViewById(R.id.note_recycler);
+        drawer_txt.setVisibility(View.VISIBLE);
+        drawer_txt.setText("Back");
+        new_note.setText("New");
+        txt_title.setText("Subjects");
+        drawer_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+//                Intent i=new Intent(getApplicationContext(),MainActivity.class);
+//                startActivity(i);
+            }
+        });
+    }
 }
