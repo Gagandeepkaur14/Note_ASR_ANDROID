70a929d32128649688790972b27b254ee1f18b76
Gagandeepkaur14 <gagankamboj14@gamil.com>
2021-02-24 01:47:55 -0500

    changes done in subject

--- app/src/main/java/com/example/note_asr_android/Adapter/SubjectAdapter.java
+++ app/src/main/java/com/example/note_asr_android/Adapter/SubjectAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -29,15 +31,38 @@ public class SubjectAdapter {
             return new Viewholder(itemview);
 
         }
-        @Override
-        public void onBindViewHolder(@NonNull Viewholder holder, final int position) {
-            holder.delete.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    deleteAddress(position);
-                }
-            });
-            holder.subject.setText(list.get(position).getSubject_name());
 
+
+
+        @Override
+        public int getItemCount() {
+            return list.size();
         }
+
+        public class Viewholder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+            TextView subject;
+            ImageView delete;
+            public Viewholder(@NonNull View itemView) {
+                super(itemView);
+                subject=(TextView)itemView.findViewById(R.id.subject_txt);
+                delete=(ImageView) itemView.findViewById(R.id.sub_delete);
+                itemView.setOnClickListener(this);
+                itemView.setOnLongClickListener(this);
+
+            }
+
+            @Override
+            public void onClick(View view) {
+                selectSubject(getAdapterPosition());
+            }
+
+            @Override
+            public boolean onLongClick(View v) {
+                editSubject(getAdapterPosition());
+                return true;
+            }
+        }
+        public abstract void deleteAddress(int i);
+        public abstract void editSubject(int i);
+        public abstract void selectSubject(int i);
 }
