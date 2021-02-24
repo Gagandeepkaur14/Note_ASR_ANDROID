84147f1698cad277d0ff11cd761641e3ff5d784c
BaljeetAmrit <72460561+AmritKaur0955@users.noreply.github.com>
2021-02-24 04:07:35 -0500

    Notes file

--- app/src/main/java/com/example/note_asr_android/Models/Notes.java
+++ app/src/main/java/com/example/note_asr_android/Models/Notes.java
@@ -1,5 +1,9 @@
 package com.example.note_asr_android.Models;
 
+import androidx.room.*;
+
+import static androidx.room.ForeignKey.CASCADE;
+
 @Entity(foreignKeys = @ForeignKey(entity = Subjects.class,parentColumns = "subject_id",childColumns = "subject_id_fk",onDelete = CASCADE))
 public class Notes {
     @PrimaryKey(autoGenerate = true)
@@ -62,5 +66,40 @@ public class Notes {
 
     public void setCreated(long created) {
         this.created = created;
+    }
+
+
+    public String getNote_audio() {
+        return note_audio;
+    }
+
+    public void setNote_audio(String note_audio) {
+        this.note_audio = note_audio;
+    }
+    public int getSubject_id_fk() {
+        return subject_id_fk;
+    }
+
+    public void setSubject_id_fk(int subject_id_fk) {
+        this.subject_id_fk = subject_id_fk;
+    }
+
+    public byte[] getNote_image() {
+        return note_image;
+    }
+
+    public void setNote_image(byte[] note_image) {
+        this.note_image = note_image;
+    }
+
+    public Notes(String description, String title, double latitude, double longitude, long created, int subject_id_fk, byte[] note_image, String note_audio) {
+        this.description = description;
+        this.title = title;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.created = created;
+        this.subject_id_fk = subject_id_fk;
+        this.note_image = note_image;
+        this.note_audio = note_audio;
     }
 }
