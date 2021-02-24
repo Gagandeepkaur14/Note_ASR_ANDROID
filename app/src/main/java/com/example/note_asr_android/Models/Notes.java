2613bbe20147fcfa771fc3d44ace76a69d22dc09
BaljeetAmrit <72460561+AmritKaur0955@users.noreply.github.com>
2021-02-24 04:05:55 -0500

    Notes file

--- app/src/main/java/com/example/note_asr_android/Models/Notes.java
+++ app/src/main/java/com/example/note_asr_android/Models/Notes.java
@@ -12,4 +12,55 @@ public class Notes {
     private int subject_id_fk;
     @ColumnInfo(typeAffinity = ColumnInfo.BLOB)
     private byte[] note_image;
+
+    // private String note_image;
+    private String note_audio;
+
+    public int getNote_id() {
+        return note_id;
+    }
+
+    public void setNote_id(int note_id) {
+        this.note_id = note_id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public long getCreated() {
+        return created;
+    }
+
+    public void setCreated(long created) {
+        this.created = created;
+    }
 }
