0d6301797c780e737ac975cf5a1dc564d28a8fc4
Gagandeepkaur14 <Gagankamboj14@gmail.com>
2021-02-24 06:00:50 -0500

    changes

--- app/src/main/java/com/example/note_asr_android/NewActivity.java
+++ app/src/main/java/com/example/note_asr_android/NewActivity.java
@@ -1,10 +1,19 @@
 package com.example.note_asr_android;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,11 +21,22 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
+import com.example.note_asr_android.Models.Notes;
+import com.example.note_asr_android.Models.Subjects;
+import com.example.note_asr_android.utils.AudioActivity;
+
+import java.io.IOException;
+import java.util.Date;
 
 public class NewActivity extends Activity {
 
     private static final int CAMERA_REQUEST = 102;
-    private static final int GALLARY_REQUEST = 101;
+    private static final int GALLERY_REQUEST = 101;
     TextView drawer_txt, new_note, txt_title, deal_txt;
     EditText title, description;
     Button record, subject;
@@ -24,7 +44,14 @@ public class NewActivity extends Activity {
     ImageView share_pic, deal_icon;
     FrameLayout share_frame;
     String path;
-
+    NotesDatabase notesDatabase;
+    Subjects selectedSubject;
+    Location userlocation;
+    LocationManager locationManager;
+    LocationListener locationListener;
+    String pathAudio;
+    Bitmap image;
+    private static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +69,7 @@ public class NewActivity extends Activity {
         share_layout = (RelativeLayout) findViewById(R.id.share_layout);
         share_pic = (ImageView) findViewById(R.id.share_pic);
         share_frame = (FrameLayout) findViewById(R.id.share_frame);
-
+        notesDatabase = NotesDatabase.getInstance(NewActivity.this);
         drawer_txt.setVisibility(View.VISIBLE);
         drawer_txt.setText("Back");
         new_note.setText("Save");
@@ -57,7 +84,107 @@ public class NewActivity extends Activity {
                 startActivity(i);
             }
         });
+        new_note.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(CheckValidation()) {
+                    Notes note;
+                    if(image != null){
 
+                        note = new Notes(description.getText().toString(),title.getText().toString(),userlocation.getLatitude(),userlocation.getLongitude(),new Date().getTime(),selectedSubject.getSubject_id(),DataConverter.convertImage2ByteArray(image),pathAudio);
+                    }
+                    else{
+                        note = new Notes(description.getText().toString(),title.getText().toString(),userlocation.getLatitude(),userlocation.getLongitude(),new Date().getTime(),selectedSubject.getSubject_id(),null,pathAudio);
+                    }
+                    notesDatabase.getNoteDao().insert(note);
+                    drawer_txt.performClick();
+                }
+            }
+        });
+
+
+   subject.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent i=new Intent(getApplicationContext(),SubjectActivity.class);
+            startActivityForResult(i, 11);
+        }
+    });
+        record.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i=new Intent(getApplicationContext(), AudioActivity.class);
+            startActivityForResult(i,112);
+        }
+    });
+        share_layout.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            selectImage();
+        }
+    });
+    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+    locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            userlocation = location;
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+        if (!hasLocationPermission())
+    requestLocationPermission();
+        else
+    startUpdateLocation();
+
+
+}
+    private void startUpdateLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, locationListener);
+    }
+
+    private void requestLocationPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+    }
+
+    private boolean hasLocationPermission() {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+    public boolean CheckValidation(){
+
+        if (title.getText().toString().trim().length() == 0) {
+            title.setError("Please enter title");
+            title.requestFocus();
+            return false;
+
+        }else if(description.getText().toString().trim().length() == 0) {
+
+            description.setError("Please enter description");
+            description.requestFocus();
+            return false;
+        }
+        else if(selectedSubject == null){
+            Toast.makeText(getApplicationContext(),"Please Select Subject",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
 
 
     }
@@ -65,6 +192,60 @@ public class NewActivity extends Activity {
     private void selectImage() {
         gallery();
 
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (REQUEST_CODE == requestCode) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, locationListener);
+            }
+        }
+    }
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+        if (reqCode == CAMERA_REQUEST && resultCode == RESULT_OK)
+        {
+            image = (Bitmap) data.getExtras().get("data");
+            share_pic.setImageBitmap(image);
+            deal_icon.setVisibility(View.GONE);
+            deal_txt.setVisibility(View.GONE);
+        }
+        else if(reqCode == 112 && resultCode == RESULT_OK){
+            pathAudio = data.getStringExtra("audio");
+        }
+        else if(reqCode == GALLERY_REQUEST && resultCode == RESULT_OK){
+
+            Uri uri = data.getData();
+            try {
+                image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
+                share_pic.setImageBitmap(image);
+                deal_icon.setVisibility(View.GONE);
+                deal_txt.setVisibility(View.GONE);
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(),e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+            }
+
+        }
+        else if (reqCode == 11){
+            if(resultCode == RESULT_OK) {
+                int subjectID = data.getIntExtra("data",-1);
+                if(subjectID != -1){
+                    for (Subjects sub: notesDatabase.getSubjectDao().getAll()) {
+                        if(sub.getSubject_id() == subjectID){
+                            selectedSubject = sub;
+                            subject.setText(sub.getSubject_name());
+                        }
+                    }
+                    //  selectedSubject =
+                }
+            }
+        }
+        else{
+
+        }
     }
 
     public void gallery() {
@@ -78,17 +259,29 @@ public class NewActivity extends Activity {
                 //  boolean result= Utility.checkPermission(ShareDeal.this);
 
                 if (items[item].equals("Take Photo")) {
+
                     CaptureImage();
+                } else if (items[item].equals("Choose from Library")) {
+                    OpenGallery();
+                } else if(items[item].equals("Cancel")){
+                    dialog.dismiss();
                 }
             }
         });
+        builder.show();
+
     }
 
-            public void CaptureImage() {
+    public void CaptureImage() {
 
-                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, CAMERA_REQUEST);
-            }
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
 
+    public void OpenGallery() {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent,GALLERY_REQUEST);
+    }
 
-        }
+}
