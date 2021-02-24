705699c1d9a85ba752a6079cced6412fe505f597
Gagandeepkaur14 <Gagankamboj14@gmail.com>
2021-02-24 06:05:35 -0500

    modified

--- app/src/main/java/com/example/note_asr_android/SubjectActivity.java
+++ app/src/main/java/com/example/note_asr_android/SubjectActivity.java
@@ -1,11 +1,24 @@
 package com.example.note_asr_android;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.note_asr_android.Adapter.SubjectAdapter;
+import com.example.note_asr_android.Models.Subjects;
 
 import java.util.List;
 
@@ -14,8 +27,8 @@ import javax.security.auth.Subject;
 public class SubjectActivity extends Activity {
     TextView drawer_txt, new_note, txt_title;
     RecyclerView recyclerView;
-    List<Subject> list;
-
+    List<Subjects> list;
+    SubjectAdapter madapter;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +42,7 @@ public class SubjectActivity extends Activity {
         drawer_txt.setText("Back");
         new_note.setText("New");
         txt_title.setText("Subjects");
-
+        list = NotesDatabase.getInstance(SubjectActivity.this).getSubjectDao().getAll();
         drawer_txt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,5 +52,137 @@ public class SubjectActivity extends Activity {
             }
         });
 
-    }
+        new_note.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            final AlertDialog.Builder mainDialog = new AlertDialog.Builder(SubjectActivity.this);
+            LayoutInflater inflater = (LayoutInflater)getApplicationContext() .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View dialogView = inflater.inflate(R.layout.subject_dialog, null);
+            mainDialog.setView(dialogView);
+
+            final Button save = (Button) dialogView.findViewById(R.id.save);
+            final ImageView cross=(ImageView) dialogView.findViewById(R.id.cross);
+            @SuppressLint("WrongViewCast") final EditText sub = (EditText) dialogView.findViewById(R.id.sub_txt);
+            final AlertDialog alertDialog = mainDialog.create();
+            alertDialog.show();
+
+            save.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //save item
+                    Subjects subject =  new Subjects(sub.getText().toString());
+
+                    NotesDatabase.getInstance(SubjectActivity.this).getSubjectDao().insert(subject);
+                    madapter.list.clear();
+                    madapter.list.addAll(NotesDatabase.getInstance(SubjectActivity.this).getSubjectDao().getAll());
+                    recyclerView.getAdapter().notifyDataSetChanged();
+                    alertDialog.dismiss();
+                }
+            });
+            cross.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    alertDialog.dismiss();
+                }
+            });
+
+        }
+    });
+
+    madapter = new SubjectAdapter(this,list) {
+        @Override
+        public void onBindViewHolder(@NonNull Viewholder holder, int position) {
+
+        }
+
+        @Override
+        public void deleteAddress(final int pos) {
+
+            final AlertDialog.Builder mainDialog = new AlertDialog.Builder(SubjectActivity.this);
+            LayoutInflater inflater = (LayoutInflater)getApplicationContext() .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View dialogView = inflater.inflate(R.layout.alert_dialog, null);
+            mainDialog.setView(dialogView);
+
+            final Button cancel = (Button) dialogView.findViewById(R.id.cancel);
+            final Button save = (Button) dialogView.findViewById(R.id.save);
+            final ImageView cross=(ImageView) dialogView.findViewById(R.id.cross);
+            final TextView cat_name=(TextView)dialogView.findViewById(R.id.cat_name);
+            final AlertDialog alertDialog = mainDialog.create();
+            alertDialog.show();
+            cat_name.setText("You want to delete this subject?");
+            cancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    alertDialog.dismiss();
+
+                }
+            });
+
+            save.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //delete item
+                    NotesDatabase.getInstance(SubjectActivity.this).getSubjectDao().delete( list.get(pos));
+                    list.remove(pos);
+                    recyclerView.getAdapter().notifyDataSetChanged();
+                    alertDialog.dismiss();
+                }
+            });
+            cross.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    alertDialog.dismiss();
+                }
+            });
+
+        }
+
+        @Override
+        public void editSubject(final int i) {
+
+            final AlertDialog.Builder mainDialog = new AlertDialog.Builder(SubjectActivity.this);
+            LayoutInflater inflater = (LayoutInflater)getApplicationContext() .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View dialogView = inflater.inflate(R.layout.subject_dialog, null);
+            mainDialog.setView(dialogView);
+
+            final Button save = (Button) dialogView.findViewById(R.id.save);
+            final ImageView cross=(ImageView) dialogView.findViewById(R.id.cross);
+            final EditText sub = dialogView.findViewById(R.id.sub_txt);
+            sub.setText(list.get(i).getSubject_name());
+            final AlertDialog alertDialog = mainDialog.create();
+            alertDialog.show();
+
+            save.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //save item
+                    Subjects subject =  list.get(i);
+                    subject.setSubject_name(sub.getText().toString());
+                    NotesDatabase.getInstance(SubjectActivity.this).getSubjectDao().update(subject);
+                    alertDialog.dismiss();
+                }
+            });
+            cross.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    alertDialog.dismiss();
+                }
+            });
+        }
+
+        @Override
+        public void selectSubject(int i) {
+            Intent intent = new Intent();
+            Subjects subject =  list.get(i);
+            intent.putExtra("data", subject.getSubject_id());
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+    };
+    RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setAdapter(madapter);
+
+}
 }
