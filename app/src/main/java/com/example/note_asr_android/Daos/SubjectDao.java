015197b8bf91c1fd9264322e96f2d37214638b49
Amandeep 98781 <kauraman98781@gmail.com>
2021-02-24 04:42:00 -0500

    Interface added

--- app/src/main/java/com/example/note_asr_android/Daos/SubjectDao.java
+++ app/src/main/java/com/example/note_asr_android/Daos/SubjectDao.java
@@ -14,5 +14,34 @@ public interface SubjectDao {
     @Query("SELECT * FROM Subjects WHERE subject_id = :id")
     List<Subjects> getSubject(int id);
 
+    /*
+     * Insert the object in database
+     * @param note, object to be inserted
+     */
+    @Insert
+    void insert(Subjects subject);
+
+    /*
+     * update the object in database
+     * @param note, object to be updated
+     */
+    @Update
+    void update(Subjects repos);
+
+    /*
+     * delete the object from database
+     * @param note, object to be deleted
+     */
+    @Delete
+    void delete(Subjects subject);
+
+    /*
+     * delete list of objects from database
+     * @param note, array of objects to be deleted
+     */
+    @Delete
+    void delete(Subjects... subject);      // Note... is varargs, here note is an array
 
 }
+
+
